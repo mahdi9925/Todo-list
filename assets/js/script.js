11bdfd6abd687fbcfcd6379f5294e1595f5c289f
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-07-17 15:21:24 +0330

    click `Add` btn change stats `Tasks` item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-function add_buttonClicked (){
-    console.log("hiiii");
+function add_buttonClicked() {
+  let tasks_counter = document.getElementById("tasks-counter");
+  tasks_counter.innerText = Number(tasks_counter.textContent) + 1;
+  console.log(tasks_counter.textContent)
+  
 }
