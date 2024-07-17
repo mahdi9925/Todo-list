73f9c489f5eca080d454fec6d043daf0dc5a77e5
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-07-17 23:32:58 +0330

    add style to `li` tasks when cursor hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ function add_buttonClicked() {
   const tasks_counter = document.getElementById("tasks-counter");
   const tasks_table = document.getElementById("tasks-table");
   const input_text = document.getElementById("input-text");
-  const add_button = document.getElementById("add-button");
   const li_tag = document.createElement("li");
+  const i_icon = document.createElement("i");
 
   if (input_text.value == "") {
     alert("Please enter a text");
@@ -13,4 +13,5 @@ function add_buttonClicked() {
     tasks_counter.innerText = Number(tasks_counter.textContent) + 1;
   }
   input_text.value = "";
+  li_tag.innerHTML += "<i class='fa fa-times' aria-hidden='true'></i>";
 }
