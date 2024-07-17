1a4a26d3f2293c7de2ce128e41277018c775355f
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-07-17 16:38:50 +0330

    Beautify & fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 function add_buttonClicked() {
-  let tasks_counter = document.getElementById("tasks-counter");
-  tasks_counter.innerText = Number(tasks_counter.textContent) + 1;
-  console.log(tasks_counter.textContent)
-  
+  const tasks_counter = document.getElementById("tasks-counter");
+  const tasks_table = document.getElementById("tasks-table");
+  const input_text = document.getElementById("input-text");
+  const add_button = document.getElementById("add-button");
+  const li_tag = document.createElement("li");
+
+  if (input_text.value == "") {
+    alert("Please enter a text");
+  } else {
+    tasks_table.appendChild(li_tag);
+    li_tag.innerText = input_text.value;
+    tasks_counter.innerText = Number(tasks_counter.textContent) + 1;
+  }
+  input_text.value = "";
 }
