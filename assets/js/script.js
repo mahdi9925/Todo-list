d162d30a39df8834f602529910f5c18be7a301c4
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-07-18 23:08:05 +0330

    `li` tag changes to `span` tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,23 @@ const tasks_counter = document.getElementById("tasks-counter");
 function add_buttonClicked() {
   const tasks_table = document.getElementById("tasks-table");
   const input_text = document.getElementById("input-text");
-  const li_tag = document.createElement("li");
+  const span_tag = document.createElement("span");
+  const check_done = document.createElement("input");
+
+  span_tag.setAttribute("id", "span-task");
+  check_done.setAttribute("type", "checkbox");
+
   if (input_text.value == "") {
     alert("Please enter a text");
   } else {
-    tasks_table.appendChild(li_tag);
-    li_tag.innerText = input_text.value;
+    tasks_table.appendChild(span_tag);
+    span_tag.innerText = input_text.value;
+
     tasks_counter.innerText = Number(tasks_counter.textContent) + 1;
   }
   input_text.value = "";
-  li_tag.innerHTML +=
+  span_tag.appendChild(check_done);
+  span_tag.innerHTML +=
     "<i class='fa fa-times' aria-hidden='true' onclick = remove_task(this)></i>";
 }
 
